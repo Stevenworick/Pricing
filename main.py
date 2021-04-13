487c6963088c882a04bca46c78f9fde883dfc20b
kevinlaventure <kevin.laventure@dauphine.eu>
2021-04-13 14:23:40 +0200

    Heston

--- main.py
+++ main.py
@@ -3,21 +3,29 @@
 from configuration import ConfigurationBuilder
 from pricing import BSM, Heston
 
-
 configuration_obj = ConfigurationBuilder(
+
+    # Common Parameters
     spot=100.0,
     strike=100.0,
     sigma=0.25,
     maturity=252,
     risk_free_rate=0,
     dividend_yield=0,
-    lt_sigma=0.15,
-    rate_reversion=0.1,
-    sigma_sigma=0.15,
-    correlation=-0.1,
-    simulation=10000,
+
+    # Heston Parameters
+    lt_sigma=0.19,
+    rate_reversion=0,
+    sigma_sigma=1.10,
+    correlation=-0.7,
+    simulation=1000,
     steps=252
 )
 
-vanilla_option = BSM(configuration_obj)
-exotic_option = Heston(configuration_obj)
+bsm_pricing = BSM(configuration_obj)
+heston_pricing = Heston(configuration_obj)
+
+print(bsm_pricing.call_price())
+print(bsm_pricing.put_price())
+print(heston_pricing.call_price())
+print(heston_pricing.put_price())
--- pricing.py
+++ pricing.py
@@ -6,73 +6,72 @@ from configuration import ConfigurationBuilder
 
 class BSM(object):
     """ Black Scholes Merton Pricing """
-    PERIODS_PER_YEAR = 365
+    PERIODS_PER_YEAR = 252
 
     def __init__(self, configuration: ConfigurationBuilder):
-        self._s = configuration.spot
-        self._k = configuration.strike
-        self._v = configuration.sigma
-        self._t = configuration.maturity / self.PERIODS_PER_YEAR
-        self._r = configuration.risk_free_rate
-        self._q = configuration.dividend_yield
-        self.d1 = (np.log(self._s / self._k) + (self._r + self._v ** 2 / 2.0) * self._t) / self._v * np.sqrt(self._t)
-        self.d2 = self.d1 - self._v * np.sqrt(self._t)
+        self.s = configuration.spot
+        self.k = configuration.strike
+        self.v = configuration.sigma
+        self.t = configuration.maturity / self.PERIODS_PER_YEAR
+        self.r = configuration.risk_free_rate
+        self.q = configuration.dividend_yield
+        self._d1 = (np.log(self.s / self.k) + (self.r + self.v ** 2 / 2.0) * self.t) / self.v * np.sqrt(self.t)
+        self._d2 = self._d1 - self.v * np.sqrt(self.t)
 
     def call_price(self):
         """ Docstring """
-        price = np.exp(-self._r * self._t) * (self._s * np.exp((self._r - self._q) * self._t) * stats.norm.cdf(
-            self.d1) - self._k * stats.norm.cdf(self.d2))
+        price = np.exp(-self.r * self.t) * (self.s * np.exp((self.r - self.q) * self.t) * stats.norm.cdf(
+            self._d1) - self.k * stats.norm.cdf(self._d2))
         return price
 
     def put_price(self):
         """ Docstring """
-        price = np.exp(-self._r * self._t) * (self._k * stats.norm.cdf(-self.d2) - (
-                    self._s * np.exp((self._r - self._q) * self._t) * stats.norm.cdf(-self.d1)))
+        price = np.exp(-self.r * self.t) * (self.k * stats.norm.cdf(-self._d2) - (
+                    self.s * np.exp((self.r - self.q) * self.t) * stats.norm.cdf(-self._d1)))
 
         return price
 
     def call_delta(self):
         """ Docstring """
-        return np.exp(-self._q * self._t) * stats.norm.cdf(self.d1)
+        return np.exp(-self.q * self.t) * stats.norm.cdf(self._d1)
 
     def put_delta(self):
         """ Docstring """
-        return np.exp(-self._q * self._t) * stats.norm.cdf(self.d1) - 1
+        return np.exp(-self.q * self.t) * stats.norm.cdf(self._d1) - 1
 
     def gamma(self):
         """ Docstring """
-        return stats.norm.pdf(self.d1) * np.exp(-self._q * self._t) / (self._s * self._v * np.sqrt(self._t))
+        return stats.norm.pdf(self._d1) * np.exp(-self.q * self.t) / (self.s * self.v * np.sqrt(self.t))
 
     def vega(self):
         """ Docstring """
-        return 0.01 * (self._s * np.sqrt(self._t) * stats.norm.pdf(self.d1) * np.exp(-self._q * self._t))
+        return 0.01 * (self.s * np.sqrt(self.t) * stats.norm.pdf(self._d1) * np.exp(-self.q * self.t))
 
     def call_rho(self):
         """ Docstring """
-        return 0.01 * (self._k * self._t * (np.exp(-self._r * self._t)) * stats.norm.cdf(self.d2))
+        return 0.01 * (self.k * self.t * (np.exp(-self.r * self.t)) * stats.norm.cdf(self._d2))
 
     def put_rho(self):
         """ Docstring """
-        return 0.01 * (-self._k * self._t * (np.exp(-self._r * self._t)) * stats.norm.cdf(-self.d2))
+        return 0.01 * (-self.k * self.t * (np.exp(-self.r * self.t)) * stats.norm.cdf(-self._d2))
 
     def call_theta(self):
         """ Docstring """
-        theta = -self._s * stats.norm.pdf(self.d1) * self._v * np.exp(-self._q * self._t) / (2 * np.sqrt(self._t)) \
-                + self._q * self._s * stats.norm.cdf(self.d1) * np.exp(-self._q * self._t) \
-                - self._r * self._k * np.exp(-self._r * self._t) * stats.norm.cdf(self.d2)
+        theta = -self.s * stats.norm.pdf(self._d1) * self.v * np.exp(-self.q * self.t) / (2 * np.sqrt(self.t)) \
+                + self.q * self.s * stats.norm.cdf(self._d1) * np.exp(-self.q * self.t) \
+                - self.r * self.k * np.exp(-self.r * self.t) * stats.norm.cdf(self._d2)
         return 1/self.PERIODS_PER_YEAR * theta
 
     def put_theta(self):
         """ Docstring """
-        theta = -self._s * stats.norm.pdf(self.d1) * self._v * np.exp(-self._q * self._t) / (2 * np.sqrt(self._t)) \
-                + self._q * self._s * stats.norm.cdf(-self.d1) * np.exp(-self._q * self._t) \
-                - self._r * self._k * np.exp(-self._r * self._t) * stats.norm.cdf(-self.d2)
+        theta = -self.s * stats.norm.pdf(self._d1) * self.v * np.exp(-self.q * self.t) / (2 * np.sqrt(self.t)) \
+                + self.q * self.s * stats.norm.cdf(-self._d1) * np.exp(-self.q * self.t) \
+                - self.r * self.k * np.exp(-self.r * self.t) * stats.norm.cdf(-self._d2)
         return 1/self.PERIODS_PER_YEAR * theta
 
 
 class Heston(BSM):
     """ Stochastic Volatility Pricing """
-    PERIODS_PER_YEAR = 365
 
     def __init__(self, configuration: ConfigurationBuilder):
         super().__init__(configuration)
@@ -83,6 +82,7 @@ class Heston(BSM):
         self._simulation = configuration.simulation
         self._steps = configuration.steps
         self._st_paths = np.zeros((self._simulation, self._steps))
+        self._prices_at_maturity = None
         self.run_simulations()
 
     @property
@@ -90,22 +90,37 @@ class Heston(BSM):
         """ Docstring """
         return self._st_paths
 
+    @property
+    def prices_at_maturity(self):
+        """ Docstring """
+        return self._prices_at_maturity
+
     def run_simulations(self):
         """ Docstring """
-        delta_t = self._t / float(self._steps)
+        delta_t = self.t / float(self._steps)
         for i in range(self._simulation):
-            vt = self._v
-            st = self._s
-            self._st_paths[i][0] = self._s
+            vt = self.v
+            st = self.s
+            self._st_paths[i][0] = self.s
             for j in range(1, self._steps):
                 w1 = np.random.normal(0, 1)
                 w2 = self._corr * w1 + np.sqrt(1 - self._corr ** 2) * np.random.normal(0, 1)
+
                 vt = (np.sqrt(max(0, vt)) + 0.5 * self._vv * np.sqrt(delta_t) * w1) ** 2 - self._rr * (
                             vt - self._lt_v) * delta_t - 0.25 * self._vv ** 2 * delta_t
 
-                st = st * np.exp((self._r - 0.5 * vt) * delta_t + np.sqrt(max(0, vt) * delta_t) * w2)
+                st = st * np.exp((self.r - self.q - 0.5 * vt) * delta_t + np.sqrt(max(0, vt) * delta_t) * w2)
                 self._st_paths[i][j] = st
+        self._prices_at_maturity = [self._st_paths[i][-1] for i in range(self._simulation)]
 
     def call_price(self):
         """ Docstring """
-        return 0
+        payoffs = [max(S - self.k, 0) for S in self._prices_at_maturity]
+        payoff = np.mean(payoffs)
+        return payoff * np.exp(-self.r * self.t)
+
+    def put_price(self):
+        """ Docstring """
+        payoffs = [max(self.k - S, 0) for S in self._prices_at_maturity]
+        payoff = np.mean(payoffs)
+        return payoff * np.exp(-self.r * self.t)
