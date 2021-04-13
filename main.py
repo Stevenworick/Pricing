09653baf37593101b39519daebea834ada51fc97
kevinlaventure <kevin.laventure@dauphine.eu>
2021-04-13 18:04:50 +0200

    GeometricBrownianMotion

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """ main """
 
 from configuration import ConfigurationBuilder
-from pricing import BSM, Heston
+from pricing import BlackScholesMerton, GeometricBrownianMotion, Heston
 
 configuration_obj = ConfigurationBuilder(
 
@@ -13,19 +13,38 @@ configuration_obj = ConfigurationBuilder(
     risk_free_rate=0,
     dividend_yield=0,
 
+    # Monte carlo Parameters
+    simulation=20000,
+    steps=252,
+
     # Heston Parameters
     lt_sigma=0.19,
-    rate_reversion=0,
+    rate_reversion=0.1,
     sigma_sigma=1.10,
-    correlation=-0.7,
-    simulation=1000,
-    steps=252
+    correlation=-0.7
 )
 
-bsm_pricing = BSM(configuration_obj)
+bsm_pricing = BlackScholesMerton(configuration_obj)
+gbm_pricing = GeometricBrownianMotion(configuration_obj)
 heston_pricing = Heston(configuration_obj)
 
+print("call")
 print(bsm_pricing.call_price())
-print(bsm_pricing.put_price())
+print(gbm_pricing.call_price())
 print(heston_pricing.call_price())
+
+print("put")
+print(bsm_pricing.put_price())
+print(gbm_pricing.put_price())
 print(heston_pricing.put_price())
+
+"""
+In[2]:
+9.94764496602258
+10.089992268159822
+9.96097452176407
+put
+9.94764496602258
+9.925691552994227
+9.876303619009215
+"""
--- pricing.py
+++ pricing.py
@@ -4,7 +4,7 @@ import scipy.stats as stats
 from configuration import ConfigurationBuilder
 
 
-class BSM(object):
+class BlackScholesMerton(object):
     """ Black Scholes Merton Pricing """
     PERIODS_PER_YEAR = 252
 
@@ -70,7 +70,40 @@ class BSM(object):
         return 1/self.PERIODS_PER_YEAR * theta
 
 
-class Heston(BSM):
+class GeometricBrownianMotion(BlackScholesMerton):
+    """ Docstring """
+    def __init__(self, configuration: ConfigurationBuilder):
+        super().__init__(configuration)
+        self.simulation = configuration.simulation
+        self.steps = configuration.steps
+        self.st_paths = np.zeros((self.simulation, self.steps))
+        self.prices_at_maturity = None
+        GeometricBrownianMotion.run_simulation(self)
+
+    def run_simulation(self):
+        """ Docstring """
+        for i in range(self.simulation):
+            self.st_paths[i][0] = self.s
+            for j in range(1, self.steps):
+                self.st_paths[i][j] = self.st_paths[i][j-1] * np.exp(
+                    (self.r - 0.5 * self.v**2) * 1 / self.PERIODS_PER_YEAR + self.v * np.sqrt(
+                        1 / self.PERIODS_PER_YEAR) * np.random.normal(0, 1))
+        self.prices_at_maturity = [self.st_paths[i][-1] for i in range(self.simulation)]
+
+    def call_price(self):
+        """ Docstring """
+        payoffs = [max(S - self.k, 0) for S in self.prices_at_maturity]
+        payoff = np.mean(payoffs)
+        return payoff * np.exp(-self.r * self.t)
+
+    def put_price(self):
+        """ Docstring """
+        payoffs = [max(self.k - S, 0) for S in self.prices_at_maturity]
+        payoff = np.mean(payoffs)
+        return payoff * np.exp(-self.r * self.t)
+
+
+class Heston(GeometricBrownianMotion):
     """ Stochastic Volatility Pricing """
 
     def __init__(self, configuration: ConfigurationBuilder):
@@ -79,30 +112,16 @@ class Heston(BSM):
         self._rr = configuration.rate_reversion
         self._vv = configuration.sigma_sigma
         self._corr = configuration.correlation
-        self._simulation = configuration.simulation
-        self._steps = configuration.steps
-        self._st_paths = np.zeros((self._simulation, self._steps))
-        self._prices_at_maturity = None
-        self.run_simulations()
-
-    @property
-    def st_paths(self):
-        """ Docstring """
-        return self._st_paths
-
-    @property
-    def prices_at_maturity(self):
-        """ Docstring """
-        return self._prices_at_maturity
-
+        Heston.run_simulation(self)
+        
     def run_simulations(self):
         """ Docstring """
-        delta_t = self.t / float(self._steps)
-        for i in range(self._simulation):
+        delta_t = self.t / float(self.steps)
+        for i in range(self.simulation):
             vt = self.v
             st = self.s
-            self._st_paths[i][0] = self.s
-            for j in range(1, self._steps):
+            self.st_paths[i][0] = self.s
+            for j in range(1, self.steps):
                 w1 = np.random.normal(0, 1)
                 w2 = self._corr * w1 + np.sqrt(1 - self._corr ** 2) * np.random.normal(0, 1)
 
@@ -110,17 +129,5 @@ class Heston(BSM):
                             vt - self._lt_v) * delta_t - 0.25 * self._vv ** 2 * delta_t
 
                 st = st * np.exp((self.r - self.q - 0.5 * vt) * delta_t + np.sqrt(max(0, vt) * delta_t) * w2)
-                self._st_paths[i][j] = st
-        self._prices_at_maturity = [self._st_paths[i][-1] for i in range(self._simulation)]
-
-    def call_price(self):
-        """ Docstring """
-        payoffs = [max(S - self.k, 0) for S in self._prices_at_maturity]
-        payoff = np.mean(payoffs)
-        return payoff * np.exp(-self.r * self.t)
-
-    def put_price(self):
-        """ Docstring """
-        payoffs = [max(self.k - S, 0) for S in self._prices_at_maturity]
-        payoff = np.mean(payoffs)
-        return payoff * np.exp(-self.r * self.t)
+                self.st_paths[i][j] = st
+        self.prices_at_maturity = [self.st_paths[i][-1] for i in range(self.simulation)]
